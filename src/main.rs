eb7e475c33d9f3a26eba38842080955b48e62b1d
Nicolas BAUW <nbauw@hotmail.com>
2024-08-21 22:11:05 +0200

    Speeds table

--- src/main.rs
+++ src/main.rs
@@ -36,27 +36,30 @@ fn main() {
     let mut starting_line: u16 = 0x95;
     let ending_line = 0xFE;
     let line_interval = 4;
+    let mut speeds: Vec<u8> = Vec::new();
     let mut rng = rand::thread_rng();
 
     while starting_line<ending_line{
     let vertical_add = rng.gen_range(0..line_interval);
-    let horizontal_pos = rng.gen_range(0x40..0xDF);
+    let horizontal_pos = rng.gen_range(0x00..0xDF);
     let color = rng.gen_range(0..3);
 
-    generate_line(starting_line, horizontal_pos, color);
+    generate_line(starting_line, horizontal_pos, color, &mut speeds);
     starting_line+=vertical_add+2;
     }
     println!("0x0000,0x0000");
+    println!("\nSpeeds table:\n{:?}", speeds);
+    println!("{} stars generated.", speeds.len());
 }
 
-fn generate_line(starting_line: u16, horizontal_pos: u16, color: u16) {
+fn generate_line(starting_line: u16, horizontal_pos: u16, color: u16, speeds: &mut Vec<u8>) {
     print!("{:#04X}", starting_line);
     print!("{:02X}", horizontal_pos);
     println!(",{:#02X}00,", starting_line+1);
     match color {
-        0 => { println!("0x8000,0x0000,"); },
-        1 => { println!("0x0000,0x8000,"); },
-        2 => { println!("0x8000,0x8000,"); },
+        0 => { println!("0x8000,0x0000,"); speeds.push(1) },
+        1 => { println!("0x0000,0x8000,"); speeds.push(2) },
+        2 => { println!("0x8000,0x8000,"); speeds.push(3) },
         _ => {}
     }
 }
@@ -64,3 +67,7 @@ fn generate_line(starting_line: u16, horizontal_pos: u16, color: u16) {
 // Sprite positions
 // horizontal left = $40 - right  = $DF
 // vertical   top  = $2C - bottom = $FE
+//
+// Sprite data word
+// vstart, hstart/2, vstop
+// color data
